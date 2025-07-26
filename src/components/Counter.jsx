44a4a80507e699e15d1f107b41947edee12a427c
shah541-g <syedabdulalishah.786@gmail.com>
2025-07-26 07:28:33 +0500

    Responsive

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -11,17 +11,17 @@ const Counter = () => {
     <div className="flex flex-col gap-8 lg:flex-row lg:gap-16 items-center justify-center min-h-[80vh] bg-gray-50 transition-colors duration-500 font-sans px-4 py-8">
       <CounterApp setHistory={setHistory} />
       <div className="hidden lg:flex h-100 w-0.5 bg-gray-900 rounded-full shadow-lg" />
-      <div className="flex flex-col w-100">
-      <h2 className="text-lg font-bold text-gray-900 text-center">
-        History
-      </h2>
-      {history.length !== 0 ? (
-        <History history={history} setHistory={setHistory} />
-      ) : (
-        <p className="mt-5 w-full max-w-lg bg-white rounded-xl shadow-lg border-2 border-gray-900 p-4 font-sans flex items-center justify-center text-lg text-gray-700 font-semibold h-100 ">
-          No history yet!
-        </p>
-      )}
+      <div className="flex flex-col w-80 sm:w-100">
+        <h2 className="text-lg font-bold text-gray-900 text-center">
+          History
+        </h2>
+        {history.length !== 0 ? (
+          <History history={history} setHistory={setHistory} />
+        ) : (
+          <p className="mt-5 w-full max-w-lg bg-white rounded-xl shadow-lg border-2 border-gray-900 p-4 font-sans flex items-center justify-center text-lg text-gray-700 font-semibold h-100 ">
+            No history yet!
+          </p>
+        )}
       </div>
     </div>
   );
