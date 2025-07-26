4cfaf0e0fb94b7d380dc1b7e8712ea1421d4561e
shah541-g <syedabdulalishah.786@gmail.com>
2025-07-26 07:36:24 +0500

    Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-center w-full py-10 bg-gray-900 shadow-lg">
       <img src="/favicon.png" width={"85px"} alt="brand-icon" className='rounded-full' />
-      <h1 className="text-center text-4xl sm:text-5xl md:text-6xl font-extrabold text-yellow-300 tracking-wide font-sans drop-shadow-lg">
+      <h1 className="text-center text-3xl sm:text-5xl md:text-6xl font-extrabold text-yellow-300 tracking-wide font-sans drop-shadow-lg">
         CounterFlow App - Challenge Accepted
       </h1>
     </div>
